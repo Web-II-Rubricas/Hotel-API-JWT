0a277e26fb4e80cce25da9676fea27b05a85a94a
RonaldoJps1228 <rsuarez13@cuc.edu.co>
2023-10-28 17:11:48 -0500

    Login

--- jwt/app.js
+++ jwt/app.js
@@ -1,13 +1,40 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import cors from "cors";
 
 const app=express()
 const port=3001
+
+//middleware
+app.use(express.json())
+app.use(cors())
 
 app.get('/api',(req,res)=>{
     res.json({
         message:"node y jwt"
     })
+})
+
+app.post('/usuario/login',(req,res)=>{
+    const usuario = req.body.usuario;
+    const clave = req.body.clave;
+
+    if(usuario == 'pedro' && clave == '123'){
+        const user={
+            id:1,
+            name:"pedro",
+            contraseña:"123"
+        }
+        jwt.sign({user},'secretkey',{expiresIn:'100s'},(error,token)=>{
+            res.cookie('token', token, { httpOnly: true })
+            res.json({ message: "inicio exitoso", user, token})
+        })
+    } else {
+        res.status(401).send('Usuario o contraseña incorrecta');
+    }
+    
+    
+    
 })
 
 app.post('/api/login',(req,res)=>{
--- react/src/components/Login.jsx
+++ react/src/components/Login.jsx
@@ -1,26 +1,51 @@
 import React from "react";
+import { useState } from 'react';
+import axios from "axios";  
 import './Login.css'; 
 
 const Login = () => {
+  const [datos, setDatos] = useState({
+    usuario:"",
+    clave:""
+  })
 
+  const handleInputChange = (e) =>{
+    const { name, value } = e.target;
+    setDatos ({...datos, [name]: value});
+  }
+
+  const handleSubmit = async(e) =>{
+    e.preventDefault();
+    if(!e.target.checkValidity()){
+      console.log("no enviar")
+    } else {
+      const res = await axios.post("http://localhost:3001/usuario/login",datos)
+      console.log(res.data)
+    }
+
+  }
     return (
         <div className="login-container">
         <h1>Login</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
             type="text"
             placeholder="Username"
             id="username"
-            name="username"
+            name="usuario"
+            onChange={handleInputChange}
+            value={datos.usuario}
           />
           <input
             type="password"
             placeholder="Password"
             id="password"
-            name="password"
+            name="clave"
+            onChange={handleInputChange}
+            value={datos.clave}
           />
-        </form>
           <button type="submit">Login</button>
+        </form>
       </div>
     )
 }    
