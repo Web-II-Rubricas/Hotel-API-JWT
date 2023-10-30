a88aa7ce586cbd28a1fa01a7afb3dc07e23df3ec
luis eduardo villa ojeda <112597598+lvilla8@users.noreply.github.com>
2023-10-30 15:03:18 -0500

    4

--- jwt/src/controllers/users.controllers.js
+++ jwt/src/controllers/users.controllers.js
@@ -1,57 +1,67 @@
-// import { json } from "express"
-// import jwt from "jsonwebtoken";
-// import cors from "cors";
+import { json } from "express"
+import jwt from "jsonwebtoken";
+import cors from "cors";
 import { pool } from "../db.js";
+import bcrypt from "bcrypt";
+
+const comparePasswords = async (inputPassword, hashedPassword) => {
+  try {
+    // Compara la contraseña proporcionada con la contraseña hash almacenada
+    return await bcrypt.compare(inputPassword, hashedPassword);
+  } catch (error) {
+    console.error('Error al comparar contraseñas:', error);
+    return false;
+  }
+};
 
 export const getUsers = async (req, res) => {
 try {
-    const [rows] = await pool.query("SELECT * FROM users");
+    const [rows] = await pool.query('SELECT * FROM users');
     res.send(rows);
 } catch (error) {
     res.status(500).json({ message: "Ha ocurrido un Error" });
 }
 };
-// app.get('/api',(req,res)=>{
-//     res.json({
-//         message:"node y jwt"
-//     })
-// })
 
 /*---------------------------------------------------------*/
 
 export const usuarioLogin = async (req, res) => {
-//     const usuario = req.body.usuario;
-//     const clave = req.body.clave;
+    const usuario = req.body.usuario;
+    const clave = req.body.clave;
+    try {
+      // Consulta la base de datos para encontrar al usuario con el nombre proporcionado
+      const query = "SELECT id, name, Contraseña FROM users WHERE name = ?";
+      const [results] = await pool.execute(query, [usuario]);
+  
+      if (results.length === 0) {
+        res.status(401).send("Usuario incorrecta");
+      } else {
+        const user = results[0];
+        const hashedPasswordFromDB = user.Contraseña;
 
-//   // Consulta la base de datos para encontrar al usuario con el nombre proporcionado
-//     const sql = "SELECT id, name, Contraseña FROM users WHERE name = ?";
-//     pool.query(sql, [usuario], (err, results) => {
-//     if (err) {
-//     console.error("Error al consultar la base de datos:", err);
-//     res.status(500).send("Error en la base de datos");
-//     } else if (results.length === 0) {
-//     res.status(401).send("Usuario o Contraseña incorrecta");
-//     } else {
-//     const user = results[0];
+        // Comprueba si la contraseña proporcionada coincide con la almacenada en la base de datos
+        const isPasswordValid = await comparePasswords(clave, hashedPasswordFromDB);
 
-//     // Comprueba si la contraseña proporcionada coincide con la almacenada en la base de datos
-//     if (user.Contraseña === clave) {
-//         // Genera un token JWT y envíalo como respuesta
-//         jwt.sign(
-//         { user },
-//         "secretkey",
-//         { expiresIn: "100s" },
-//         (error, token) => {
-//             res.cookie("token", token, { httpOnly: true });
-//             res.json({ message: "inicio exitoso", user, token });
-//         }
-//         );
-//     } else {
-//         res.status(401).send("Usuario o Contraseña incorrecta");
-//       }
-//     }
-//   });
+        if (isPasswordValid) {
+          // Genera un token JWT y envíalo como respuesta
+          jwt.sign({ user }, "secretkey", { expiresIn: "100s" }, (error, token) => {
+            if (error) {
+              res.status(500).send("Error al generar el token");
+            } else {
+              res.cookie("token", token, { httpOnly: true });
+              res.json({ message: "inicio exitoso", user, token });
+            }
+          });
+        } else {
+          res.status(401).send("Contraseña incorrecta");
+        }
+      }
+    } catch (error) {
+      console.error("Error al consultar la base de datos:", error);
+      res.status(500).send("Error en la base de datos");
+    }
 };
+
 // app.post('/usuario/login',(req,res)=>{
 //     const usuario = req.body.usuario;
 //     const clave = req.body.clave;
@@ -73,50 +83,16 @@ export const usuarioLogin = async (req, res) => {
 
 /*---------------------------------------------------------*/
 
-export const apiLogin = (req, res) => {
-  // console.log(req.body);
-  // res.send('post Login');
-};
-// app.post('/api/login',(req,res)=>{
-//     const user={
-//         id:1,
-//         name:"pedro",
-//         email:"pedro@gmail.com"
-//     }
-//     jwt.sign({user},'secretkey',{expiresIn:'100s'},(error,token)=>{
-//         res.json({token})
-//     })
+export function verifyToken(req,res,next){
+    const bearerHeader=req.headers['authorization']
 
-// })
+    if (typeof bearerHeader !=='undefined') {
+        const bearerToken=bearerHeader.split(" ")[1]
+        req.token=bearerToken
 
-/*---------------------------------------------------------*/
+        next()
+    } else {
 
-export const apiProtected = async (req, res) => {
-};
-// app.post('/api/protected',verifyToken,(req,res)=>{
-//     jwt.verify(req.token,'secretkey',(error,authData)=>{
-//         if (error) {
-//             res.sendStatus(403)
-//         } else {
-//             res.json({
-//                 message:"Acceso a ruta protegida",
-//                 authData
-//             })
-//         }
-//     })
-
-// })
-
-// function verifyToken(req,res,next){
-//     const bearerHeader=req.headers['authorization']
-
-//     if (typeof bearerHeader !=='undefined') {
-//         const bearerToken=bearerHeader.split(" ")[1]
-//         req.token=bearerToken
-
-//         next()
-//     } else {
-
-//         res.sendStatus(403)
-//     }
-// }
+        res.sendStatus(403)
+    }
+}
--- jwt/src/routes/users.rutas.js
+++ jwt/src/routes/users.rutas.js
@@ -1,11 +1,23 @@
 import { Router } from "express";
-import { getUsers, usuarioLogin, apiLogin, apiProtected} from "../controllers/users.controllers.js";
+import { getUsers, usuarioLogin, verifyToken} from "../controllers/users.controllers.js";
 
 const router=Router()
 
 router.get('/users',getUsers)
-router.post('/users',usuarioLogin)
-router.post('/users/login',apiLogin)
-router.post('/users/protected',apiProtected)
+
+router.post('/users/login',usuarioLogin)
+
+router.post('/users/protected', verifyToken, (req,res)=>{
+    jwt.verify(req.token,'secretkey',(error,authData)=>{
+        if (error) {
+            res.sendStatus(403)
+        } else {
+            res.json({
+                message:"Acceso a ruta protegida",authData
+            })
+        }
+    })
+})
+
 
 export default router
