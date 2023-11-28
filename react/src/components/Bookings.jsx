d19633fcf0b4c2880151b578b25b37e33e5614a9
luis eduardo villa ojeda <112597598+lvilla8@users.noreply.github.com>
2023-11-28 09:51:21 -0500

    F

--- react/src/components/Bookings.jsx
+++ react/src/components/Bookings.jsx
@@ -6,12 +6,12 @@ import './Login.css'
 const Bookings = () => {
   const [reservas, setReservas] = useState([]);
   const [datos, setDatos] = useState({
-    codigoH: '',
-    nombre: '',
-    telefono: '',
-    fechadereserva: '',
-    fechaEntrada: '',
-    fechaSalida: '',
+    codigo_habitacion: "",
+    nombre_cliente: "",
+    telefono_cliente: "",
+    fecha_reservacion: "",
+    fecha_entrada: "",
+    fecha_salida: "",
   });
 
   useEffect(() => {
@@ -21,52 +21,86 @@ const Bookings = () => {
   }, []);
 
   const handleInputChange = (e) => {
-    setDatos({ ...datos, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+    setDatos ({...datos, [name]: value});
   };
 
-  const handleReservaSubmit = (e) => {
+  const handleReservaSubmit = async (e) => {
     e.preventDefault();
-
-    axios.post('http://localhost:5000/bookings', datos)
-      .then(response => {
-        setReservas([...reservas, response.data]);
+    if(!e.target.checkValidity()){
+      console.log("no enviar")
+    } else {
+  const res= await axios.post('http://localhost:5000/bookings', datos)
+      .then((res) => {
+        // console.log(res.data)
+        // setReservas([...reservas, response.data]);
       })
       .catch(error => console.error('Error al crear la reserva', error));
+    }
+  };
+  const handleUpdate = async(e) =>{
+    e.preventDefault();
+    if(!e.target.checkValidity()){
+      console.log("no enviar")
+    } else {
+      const res = await axios.patch(`http://localhost:5000/bookings/${datos.codigo}`,datos)
+    .then((res) => {
+    console.log(res.data)
+    })
+    .catch((error) => {
+    console.log('Credenciales incorrectas: ' + error.Rooms);
+    });
+    }
+  }
+
+  const handleDelete = async () => {
+    try { 
+      const res = await axios.delete(`http://localhost:5000/bookings/${datos.id}`);
+      console.log('Deleted:', res.data);
+    } catch (error) {
+      console.log('Error deleting:', error.response.data.message);
+    }
   };
 
   return (
     <div>
       <h1>Reservas</h1>
 
-      <ul>
-        {reservas.map(reserva => (
-          <li key={reserva.codigo}>
-          {reserva.codigo_habitacion} - {reserva.nombre_cliente} - {reserva.telefono_cliente}  - {reserva.fecha_reservacion} -{reserva.fecha_entrada} - {reserva.fecha_salida}
-          </li>
-        ))}
-      </ul>
-
       <form onSubmit={handleReservaSubmit}>
+      {/* <label>Codigo D:</label>
+        <input type="number" name="codigo" id="codigo" value={datos.codigo} onChange={handleInputChange} /> */}
+
       <label>Numero de habitacion:</label>
-        <input type="number" name="codigoH" onChange={handleInputChange} />
+        <input type="number" name="codigo_habitacion" id="codigo_habitacion" value={datos.codigo_habitacion} onChange={handleInputChange} />
 
         <label>Nombre:</label>
-        <input type="text" name="nombre" onChange={handleInputChange} />
+        <input type="text" name="nombre_cliente" id="nombre_cliente" value={datos.nonombre_cliente} onChange={handleInputChange} />
 
         <label>Teléfono:</label>
-        <input type="number" name="telefono" onChange={handleInputChange} />
+        <input type="number" name="telefono_cliente" id="telefono_cliente" value={datos.telefono_cliente} onChange={handleInputChange} />
 
         <label>Fecha de reservacion:</label>
-        <input type="number" name="fechadereserva" onChange={handleInputChange} />
+        <input type="number" name="fecha_reservacion" id="fecha_reservacion" value={datos.fecha_reservacion} onChange={handleInputChange} />
 
         <label>Fecha de entrada:</label>
-        <input type="number" name="fechaEntrada" onChange={handleInputChange} />
+        <input type="number" name="fecha_entrada" id="fecha_entrada" value={datos.fecha_entrada} onChange={handleInputChange} />
 
         <label>Fecha de salida:</label>
-        <input type="number" name="fechaSalida" onChange={handleInputChange} />
+        <input type="number" name="fecha_salida" id="fecha_salida" value={datos.fecha_salida} onChange={handleInputChange} />
 
         <button type="submit">Reservar</button>
       </form>
+        {/* <button onClick={handleUpdate} className="update-button">Actualizar</button>
+        <button onClick={handleDelete} className="delete-button">Eliminar</button> */}
+      <ul>
+        {reservas.map(reserva => (
+          <li key={reserva.codigo}>
+              {/* {reserva.codigo} */}
+               - {reserva.codigo_habitacion} - {reserva.nombre_cliente} - {reserva.telefono_cliente}  -
+              {reserva.fecha_reservacion} -{reserva.fecha_entrada} - {reserva.fecha_salida}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
